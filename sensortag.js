45978213ba81cc47ddd2a26aee1b824baec61b2b
oroce <robert@oroszi.net>
2014-09-06 15:35:01 +0200

    added watch ability, and better filtering

--- sensortag.js
+++ sensortag.js
@@ -1,18 +1,36 @@
 var producer = require('godot-producer');
 var Tist = require('tist');
+var groupBy = require('group-by');
+var debug = require('debug')('godot-sensortag');
 
 var SensorTagProducer = module.exports = producer(
   function constructor(options) {
     options || (options = {});
+    options.tistOptions || (options.tistOptions = {});
+
+    this.watch = options.tistOptions.watch;
+    this.sensors = options.tistOptions.sensors || [];
     var self = this;
 
-    this.metrics = {};
+    if (options.metricTags !== false) {
+      this.metricTags = [].slice.call(options.metricTags||SensorTagProducer.metricTags);
+    }
+    this.metrics = [];
+    this.devices = [];
     this.mappings = options.mappings||{};
     this.tist = options.tist || new Tist(options.uuids, options.tistOptions);
     this.tist.on('device', function onTistDevice(device) {
+      debug('device arrived %s', device.uuid)
+      self.devices.push(device);
+      device.once('disconnect', function() {
+        var index = self.devices.indexOf(device);
+        self.devices.slice(index, 1);
+        device.removeListener('metric');
+        device = null;
+      });
       device.on('metric', function onMetric(name, value) {
-        self.metrics[name] || (self.metrics[name] = []);
-        self.metrics[name].push({
+        self.metrics.push({
+          sensor: name,
           value: value,
           when: Date.now(),
           device: device.uuid
@@ -22,25 +40,72 @@ var SensorTagProducer = module.exports = producer(
   },
   function produce() {
     var self = this;
-    var metrics = this.metrics;
+    var send = function(metrics) {
+      var events = toEvents(metrics, self.mappings, self.values, self.metricTags);
+      events.forEach(function(data) {
+        debug('emitting data %j', data);
+        self.emit('data', data);
+      });
+    };
 
-    Object.keys(metrics).forEach(function(sensor) {
-      var metric = metrics[sensor];
-      var average = metric.reduce(function (prev, row) {
-        return (prev + row.value) / 2;
-      }, 0);
-      var device = metric[0].device;
+    if (this.watch === false && this.devices.length === 0) {
+      debug('no device yet, nothing to crunch');
+      return;
+    }
 
-      self.emit('data', {
+    if (this.watch === false) {
+      this.devices.forEach(function(device) {
+        this.sensors.forEach(function(sensor) {
+          device.read(sensor, function(err, metrics) {
+            var list = metrics.map(function(metric) {
+              return {
+                sensor: metric.name,
+                value: metric.value,
+                when: Date.now(),
+                device: device.uuid
+              };
+            });
+            send(list);
+          });
+        });
+      }, this);
+    } else {
+      send(this.metrics);
+    }
+
+    this.metrics = [];
+  }
+);
+
+function toEvents(metrics, mappings, values, metricsTags) {
+  debug('crunching metrics %j', metrics);
+  var list = [];
+  var byDevices = groupBy(metrics, 'device'); 
+  Object.keys(byDevices).forEach(function(device) {
+    var bySensors = groupBy(byDevices[device], 'sensor')
+    Object.keys(bySensors).forEach(function(sensor) {
+      var sensorValues = bySensors[sensor];
+      var average;
+      if (sensorValues.length === 1) {
+        average = sensorValues[0].value;
+      } else {
+        average = sensorValues.reduce(function (prev, row) {
+          return (prev + row.value) / 2;
+        }, 0);
+      }
+
+      var data = {
         service: sensor,
-        host: self.mappings[device]||device,
+        host: mappings[device]||device,
         meta: {
           uuid: device,
         },
+        tags: (values.tags||[]).concat(metricsTags),
         metric: average
-      });
+      };
+      list.push(data);
     });
-
-    this.metrics = {};
-  }
-);
+  });
+  return list;
+}
+SensorTagProducer.metricTags = ['st-metric'];
